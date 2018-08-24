c1f455362a767048cf077e49c16c592edd918c8a
azuse <misakaxindex@gmail.com>
2018-08-24 14:51:39 +0800

    debug

--- autosave.php
+++ autosave.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-$time = date("Y-m-d H:s:i");
+$time = date("Y-m-d H:i:s");
 if($_SERVER['REQUEST_METHOD']=='POST'){
     $file = fopen("autosave.log", "w");
     fwrite($file,$_POST["text"]);
