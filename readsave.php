150240f447c540a657ee0abdf13facd07a343cbc
azuse <misakaxindex@gmail.com>
2018-08-24 15:35:42 +0800

    debuug

--- readsave.php
+++ readsave.php
@@ -3,7 +3,10 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 $file = fopen("autosave.log", "r");
-echo fread($file,filesize("autosave.log"));
+if(filesize("autosave.log") > 0)
+    echo fread($file,filesize("autosave.log"));
+else
+    echo "";
 fclose($file);
 
 ?>
